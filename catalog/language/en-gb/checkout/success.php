9cd39b19dca5dfa2c419972268df4f1c32ebd1b5
Yehor <158765585+Carisky@users.noreply.github.com>
2025-07-12 16:50:53 +0200

    Show spam folder reminder on order success

--- catalog/language/en-gb/checkout/success.php
+++ catalog/language/en-gb/checkout/success.php
@@ -10,3 +10,4 @@ $_['text_checkout']        = 'Checkout';
 $_['text_success']         = 'Success';
 $_['text_customer']        = '<p>Your order has been successfully processed!</p><p>You can view your order history by going to the <a href="%s">my account</a> page and by clicking on <a href="%s">history</a>.</p><p>If your purchase has an associated download, you can go to the account <a href="%s">downloads</a> page to view them.</p><p>Please direct any questions you have to the <a href="%s">store owner</a>.</p><p>Thanks for shopping with us online!</p>';
 $_['text_guest']           = '<p>Your order has been successfully processed!</p><p>Please direct any questions you have to the <a href="%s">store owner</a>.</p><p>Thanks for shopping with us online!</p>';
+$_['text_spam_warning']    = 'Sometimes emails can end up in your spam folder. Please check it if you do not see our email.';
